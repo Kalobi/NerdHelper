b0e1db019fe4a463b9575740bcc177121fc81566
Kalobi <46748261+Kalobi@users.noreply.github.com>
2024-01-26 17:50:52 +0100

    Port to .NET 7.0

--- Scripts/fix_old_spikes.jl
+++
@@ -1,8 +0,0 @@
-for room in loadedState.side.map.rooms
-	for entity in room.entities
-        if startswith(entity.name, "NerdHelper/DashThroughSpikes") && haskey(entity.data, "textures")
-            entity.data["type"] = entity.data["textures"]
-            delete!(entity.data, "textures")
-        end
-    end
-end
--- zipnerdhelper.bat
+++
@@ -1,2 +0,0 @@
-DEL ..\NerdHelper.zip
-"D:\Program Files\7-Zip\7z.exe" a -tzip ..\NerdHelper.zip -r Ahorn\* Loenn\* DLL\NerdHelper.dll Graphics\* everest.yaml
